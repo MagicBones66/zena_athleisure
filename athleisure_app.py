5f790492b2bd2290802311058b433b368e4f2dd5
MagicBones66 <125248651+MagicBones66@users.noreply.github.com>
2023-02-21 07:21:15 +0000

    Update athleisure_app.py

--- athleisure_app.py
+++ athleisure_app.py
@@ -1,1 +1,8 @@
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
+streamlit.title('Zena's Amazing Athleisure Catalog')
+# streamlit.header('Breakfast Favourites')
